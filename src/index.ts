ea790a97d79cf1ab4edc1a577405585e64c7f265
mar1332244 <marcelo1332244@gmail.com>
2022-02-07 21:40:33 -0800

    will continue tommorow

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ dotenv.config();
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_INTEGRATIONS
     ]
 });
 
 client.on("ready", () => {
-    console.log("ready");
+    console.log(`Logged in as ${client.user?.tag}!`);
 });
 
 client.on("messageCreate", (msg) => {
@@ -21,6 +22,11 @@ client.on("messageCreate", (msg) => {
             content: "hey",
         });
     }
+});
+
+
+client.on('guildMemberAdd', (member) => {
+    console.log(member);
 });
 
 client.login(process.env.TOKEN);
