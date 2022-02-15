f18b1ab65db3732c15712a3e82e8fb7d6775825c
mar1332244 <marcelo1332244@gmail.com>
2022-02-14 19:20:14 -0800

    its beautiful

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
-import DiscordJS, { Intents } from "discord.js";
+import DiscordJS, { Intents, MessageEmbed } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+/*
+// .setImage()
+const greeting = new MessageEmbed({
+    image: { 
+        url: "./images/greeting.jpg"
+    }
+});
+*/
+
+// const allIntents = new Intents(32767);  // all intents
 const client = new DiscordJS.Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_INTEGRATIONS
+        Intents.FLAGS.GUILD_MEMBERS,
     ]
 });
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
+    console.log(`Logged in as ${client.user?.tag}`);
 });
-
-client.on("messageCreate", (msg) => {
-    
-    if (msg.content == "hey")
-    {
-        msg.reply({
-            content: "hey",
-        });
-    }
-});
-
 
 client.on('guildMemberAdd', (member) => {
-    console.log(member);
+    member.send({
+        files: ["./images/greeting.jpg"]
+    });
 });
 
 client.login(process.env.TOKEN);
